af9ddda815ad243c52e21a6fc232d6fcaae126fa
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 13:28:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index{|name,i| name}
+  dwarves.each_with_index{|name,idx| puts "#{idx}. #{name}}
 end
 
 def summon_captain_planet# code an argument here
